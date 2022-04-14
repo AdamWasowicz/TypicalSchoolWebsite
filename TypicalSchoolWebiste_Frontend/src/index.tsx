5193f119a87377544ed4d17fad0b98591a036117
Adam Wąsowicz <adam.p.wasowicz@gmail.com>
2022-04-14 14:06:55 +0200

    Redux Infrastructure

--- TypicalSchoolWebiste_Frontend/src/index.tsx
+++ TypicalSchoolWebiste_Frontend/src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { store } from './redux/store';
 import App from './components/App/App';
+
+
 
 const root = document.getElementById('root')! as HTMLElement;
 
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   root
 );
 
--- TypicalSchoolWebiste_Frontend/src/pages/routes.ts
+++ TypicalSchoolWebiste_Frontend/src/pages/routes.ts
@@ -1,11 +1,20 @@
-import Home from "./Home";
 import { IRoute } from '../types/route';
+
+//Routes
+import Home from "./Home";
+import ReduxTest from './ReduxTest';
 
 
 export const routes: IRoute[] = [
     {
         route: '/',
         module: Home,
+        roles: ['ROLE_USER', 'ROLE_ADMIN', 'NOT_LOGGED'],
+    },
+
+    {
+        route: '/reduxTest',
+        module: ReduxTest,
         roles: ['ROLE_USER', 'ROLE_ADMIN', 'NOT_LOGGED'],
     },
 
